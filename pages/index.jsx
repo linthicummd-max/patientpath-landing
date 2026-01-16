b822de7bb4b62ae51e18d42853cba77fb98bc87d
linthicummd-max <linthicummd@gmail.com>
2026-01-16 09:27:51 -0800

    Fix JSX syntax - correct pages/index.jsx formatting

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
-      return (
-              <main>
-                    <h1>Patient Path</h1>h1>
-                    <p>Intelligent Post-Surgical Care Communications</p>p>
-              </main>main>
-            );
-}</main>
+        return (
+                  <main style={{ padding: '40px' }}>
+                              <h1>Patient Path</h1>h1>
+                        <p>Intelligent Post-Surgical Care Communications</p>p>
+                  </main>main>
+                );
+}</h1>
