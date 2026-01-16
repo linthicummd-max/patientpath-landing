079a3a10c31c31febe12499f3bf425efd805a75e
linthicummd-max <linthicummd@gmail.com>
2026-01-16 09:30:39 -0800

    Delete pages/index.jsx

--- pages/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-        return (
-                  <main style={{ padding: '40px' }}>
-                              <h1>Patient Path</h1>h1>
-                        <p>Intelligent Post-Surgical Care Communications</p>p>
-                  </main>main>
-                );
-}</h1>
